321b0783ce0a6f2bce69ca07a2f0da9114dac8bb
sujeong <kireystal0708@gmail.com>
2019-08-22 20:00:23 +0900

    like 폴더 나누기

--- Izenstargram/app/src/main/java/com/example/izenstargram/MainActivity.java
+++ Izenstargram/app/src/main/java/com/example/izenstargram/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.izenstargram;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -15,7 +13,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
+
+import com.example.izenstargram.like.LikeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
--- Izenstargram/app/src/main/java/com/example/izenstargram/like/LikeFragment.java
+++ Izenstargram/app/src/main/java/com/example/izenstargram/like/LikeFragment.java
@@ -1,4 +1,4 @@
-package com.example.izenstargram;
+package com.example.izenstargram.like;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.izenstargram.R;
 
 public class LikeFragment extends Fragment {
 
