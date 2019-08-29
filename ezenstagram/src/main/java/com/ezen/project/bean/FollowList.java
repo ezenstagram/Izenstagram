f22c47cd5916d5413d9204573d7de1ad1bf29a66
jiyoonhong <hongji13@naver.com>
2019-08-29 18:41:59 +0900

    프로필 수정

---
+++ ezenstagram/src/main/java/com/ezen/project/bean/FollowList.java
@@ -0,0 +1,40 @@
+package com.ezen.project.bean;
+
+public class FollowList {
+	private boolean followStatus;
+	 private int user_id;		
+	 private String name;	 
+	 private String profile_photo;
+	 private String login_id;
+	 
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	public boolean isFollowStatus() {
+		return followStatus;
+	}
+	public void setFollowStatus(boolean followStatus) {
+		this.followStatus = followStatus;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfile_photo() {
+		return profile_photo;
+	}
+	public void setProfile_photo(String profile_photo) {
+		this.profile_photo = profile_photo;
+	}	 
+}
